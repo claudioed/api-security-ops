6f53c8906ad3d6125bbb12b572f68137832ec83e
Luram Archanjo <luram.archanjo@mercadolivre.com>
2021-04-12 07:53:29 -0300

    Improvements

--- play/play-all.sh
+++ play/play-all.sh
@@ -2,4 +2,6 @@
 kubectl apply -f namespace
 kubectl apply -f deployments -n games
 kubectl apply -f services -n games
+sleep 10
+kubectl apply -f apis -n games
 kubectl annotate service play ingress.kubernetes.io/service-upstream=true -n games
